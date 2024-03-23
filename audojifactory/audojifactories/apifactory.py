088863628aa8c738ee3b6f507e426f39bf8cd411
Ugochukwu Nwachukwu <danielnwachukwu5738@gmail.com>
2024-03-23 03:37:50 +0000

    Update: worked on apifactory

--- audojifactory/audojifactories/apifactory.py
+++ audojifactory/audojifactories/apifactory.py
@@ -3,6 +3,7 @@ import json
 import os
 import re
 import tempfile
+import shutil
 
 import librosa
 import openai
@@ -30,7 +31,7 @@ class AudioProcessor:
         self.group_name = group_name
         self.audio_file_instance = audio_file_instance
         self.audio_path = audio_file_instance.audio_file.url
-        self.audio_path = "https://audojistore.s3.amazonaws.com/media/audio_files/Sam-Smith-Man-I-Am_iSrFxoN.mp3"
+        self.temp_audio_path = self.download_audio(self.audio_path)
 
     async def send_segment_to_group(self, segment_data):
         channel_layer = get_channel_layer()
@@ -42,23 +43,36 @@ class AudioProcessor:
             },
         )
 
+    def download_audio(self, audio_url):
+        """Download audio from URL to a temporary file and return the file path."""
+        # Download the audio file content from the URL
+        response = requests.get(audio_url, stream=True)
+        
+        # Create a temporary file to hold the audio data
+        with tempfile.NamedTemporaryFile(suffix=".mp3", delete=False) as self.temp_audio_file:
+            shutil.copyfileobj(response.raw, self.temp_audio_file)
+            
+            # Ensure the file is saved before proceeding
+            self.temp_audio_file.flush()
+        return self.temp_audio_file.name
+
+    async def cleanup(self):
+        """Remove the temporary audio file."""
+        if self.temp_audio_path and os.path.exists(self.temp_audio_path):
+            os.remove(self.temp_audio_path)
+
     async def transcribe_audio(self):
         try:
             # Download the audio file content from the URL
-            audio_content = requests.get(self.audio_path).content
+            response = requests.get(self.audio_path, stream=True)
             
-            # Use a temporary file to hold the audio data
-            with tempfile.NamedTemporaryFile(delete=True) as temp_audio_file:
-                temp_audio_file.write(audio_content)
-                temp_audio_file.seek(0) 
-            
-                # transcript = self.client.audio.transcriptions.create(model="whisper-1", file=audio_file, response_format="vtt")
-                transcript = openai_client.audio.transcriptions.create(
-                    file=temp_audio_file.name,
-                    model="whisper-1",
-                    response_format="verbose_json",
-                    timestamp_granularities=["word", "segment"],
-                )
+            # transcript = self.client.audio.transcriptions.create(model="whisper-1", file=audio_file, response_format="vtt")
+            transcript = await openai_client.audio.transcriptions.create(
+                file=open(self.temp_audio_path, 'rb'),
+                model="whisper-1",
+                response_format="verbose_json",
+                timestamp_granularities=["word", "segment"],
+            )
             return transcript
         except Exception as e:
             logger.error(f"Error during transcription: {e}")
@@ -85,7 +99,7 @@ class AudioProcessor:
                 response_format={"type": "json_object"},
             )
             processed_response = json.loads(response.choices[0].message.content)
-            logger.info(f"This is the response: {processed_response}")
+            logger.info(f"Categorization response: {processed_response}")
             category = processed_response.get("category", None)
             return category
         except openai.APIError as e:
@@ -113,25 +127,30 @@ class AudioProcessor:
         h, m, s = map(float, time_str.split(":"))
         return int((h * 3600 + m * 60 + s) * 1000)
 
-    async def process_and_save_segments(self, transcript, audio_file_instance):
+    async def seconds_to_milliseconds(self, seconds):
+        """Converts seconds to milliseconds."""
+        return int(seconds * 1000)
+
+    async def process_and_save_segments(self, transcript_result):
         logger.info("Processing Started")
         segments_data = []
 
         # vtt_content = transcript
         # segments = self.parse_vtt(vtt_content)
 
-        audio = AudioSegment.from_file(self.audio_path)
+        audio = AudioSegment.from_file(open(self.temp_audio_path, 'rb'))
 
-        for i, segment in enumerate(transcript.segments):
+        for i, segment in enumerate(transcript_result.segments):
             start = segment.get("start")
             end = segment.get("end")
             text = segment.get("text", "").strip()
-
+            
             category = await self.analyze_category_async(text)
-
-            start_ms, end_ms = self.convert_to_ms(start), self.convert_to_ms(end)
+            
+            start_ms = await self.seconds_to_milliseconds(start)
+            end_ms = await self.seconds_to_milliseconds(end)
             segment_audio = audio[start_ms:end_ms]
-
+            
             # Create a segment instance and save it to the database
             segment_data = {
                 "audio_file": self.audio_file_instance,
@@ -140,26 +159,25 @@ class AudioProcessor:
                 "transcription": text,
                 "category": category,
             }
-            print("This is the segment data: ")
-            print(segment_data)
-            # audio_segment_instance = AudioSegmentModel(**segment_data)
+            
+            audio_segment_instance = AudioSegmentModel(**segment_data)
 
-            # await sync_to_async(audio_segment_instance.save)()
+            await sync_to_async(audio_segment_instance.save)()
 
             # ==================== Create Audoji ====================
-            # create_audoji_sync = sync_to_async(
-            #     AudioRetrieval(audio_segment_instance, start, end).create_audoji
-            # )
-            # created_audoji = await create_audoji_sync()
+            create_audoji_sync = sync_to_async(
+                AudioRetrieval(audio_segment_instance, start, end).create_audoji
+            )
+            created_audoji = await create_audoji_sync()
 
-            # logger.info(f"Audoji created! {created_audoji}")
+            logger.info(f"Audoji created! {created_audoji}")
             # ==================== Create Audoji ====================
 
             segment_data["audio_file_id"] = self.audio_file_instance.id
 
-            # await self.send_segment_to_group(
-            #     AudioSegmentSerializer(audio_segment_instance).data
-            # )
+            await self.send_segment_to_group(
+                AudioSegmentSerializer(audio_segment_instance).data
+            )
 
             logger.info(
                 f"Segment {i} exported and saved: Text: {text} | Start - {start_ms}ms, End - {end_ms}ms"
@@ -171,9 +189,9 @@ class AudioProcessor:
         logger.info("Run operation started!")
 
         transcription_result = await self.transcribe_audio()
-        print("This is the transcription result: ")
-        print(transcription_result)
-        # return await self.process_and_save_segments(transcription_result)
+        result = await self.process_and_save_segments(transcription_result)
+        await self.cleanup()
+        return result
 
 
 # Usage example
